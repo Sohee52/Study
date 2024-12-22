68e318fdfe6d78ecf82898289917eed3eea0f3c8
unknown <wneh2023@naver.com>
2024-12-22 18:24:32 +0900

    third commit

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
-my_set={1,2,3,3,3}
-print(my_set)
+java={'푸','피글렛','티거'}
+python=set(['푸','이요르'])
+# print(java&python)
+# print(java.intersection(python))
+# print(java|python)
+# print(java.union(python))
+# print(java-python)
+# print(java.difference(python))
+python.add('피글렛')
+# print(python)
+java.remove('피글렛')
+print(java)
